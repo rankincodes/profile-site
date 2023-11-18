a55f9ca1ca58b07926ebf2276a564c43cf10825d
rankincodes <314112@protonmail.com>
2023-11-18 15:30:29 -0500

    Image switch on dark mode

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ import './style.css'
 
 document.addEventListener('DOMContentLoaded', () => {
   const checkbox = document.getElementById('checkbox');
+  const bannerImage = document.getElementById('banner');
 
   checkbox.addEventListener('change', function() {
     if(this.checked) {
       // Dark mode
       document.documentElement.setAttribute('data-theme', 'dark');
+      bannerImage.src = 'banner-dark.png'
     } else {
       // Light mode
       document.documentElement.setAttribute('data-theme', 'light');
+      bannerImage.src = 'banner-light.png'
     }
   });
 });
